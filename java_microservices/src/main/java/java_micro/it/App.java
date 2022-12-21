a686b4fc547b3663e06d389d43b564d995760ec9
Gianluca <circelli93@gmail.com>
2022-12-21 11:55:58 +0100

    creazione progetto jakarta

---
+++ java_microservices/src/main/java/java_micro/it/App.java
@@ -0,0 +1,13 @@
+package java_micro.it;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
