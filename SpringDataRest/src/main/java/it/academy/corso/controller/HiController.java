fe1d49b30f4ae032adc81307e837f4fb59e5c794
Gianluca <circelli93@gmail.com>
2022-12-23 17:05:37 +0100

    SpringDataRest esercizi

---
+++ SpringDataRest/src/main/java/it/academy/corso/controller/HiController.java
@@ -0,0 +1,19 @@
+package it.academy.corso.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HiController {
+
+    @GetMapping("/hola")
+    public ResponseEntity<String> hola (){
+        String message = "Hola Gianluca";
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+}
